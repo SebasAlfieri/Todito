b68bfe2c5ea4838b401cd4d7835da83fb1385ae5
Sebastian Daniel Alfieri <86760582+SebasAlfieri@users.noreply.github.com>
2022-05-09 15:07:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -43,5 +43,109 @@ if (x==2){
 if (x==3){
     document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
 }
+
+if (x==0){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==1){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==2){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==3){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==0){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==1){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==2){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==3){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==4){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==5){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==6){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==7){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==8){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==9){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==10){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==11){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==12){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==13){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==14){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==15){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==16){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==17){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==18){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==19){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==20){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+
+if (x==21){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
 }
 
