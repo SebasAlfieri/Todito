e4ceee99cbd6987deeff017e6eedd25596681926
Sebastian Daniel Alfieri <86760582+SebasAlfieri@users.noreply.github.com>
2022-05-09 11:27:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+element = document.getElementById("press");
+
+// Fondo todito
+
+element.addEventListener("click", function(e) {
+    e.preventDefault;
+    
+    element.classList.remove("animation");
+
+    element.offsetWidth = element.offsetWidth;
+
+    element.classList.add("animation");
+}, false);
+
+
+// Cartas Randomizer
+
 var button = document.getElementById("press");
 
 button.addEventListener("click", function(){
@@ -9,17 +26,22 @@ function randomBgImage() {
     var bgImage = 'url(./img/' + x + '.png)';
     console.log(bgImage);
 
-    document.getElementById("card").style.backgroundImage = bgImage;
+    document.getElementById("flip-card-front").style.backgroundImage = bgImage;
+
+if (x==0){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
 }
 
-element = document.getElementById("press");
+if (x==1){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
 
-element.addEventListener("click", function(e) {
-    e.preventDefault;
-    
-    element.classList.remove("animation");
+if (x==2){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
 
-    element.offsetWidth = element.offsetWidth;
+if (x==3){
+    document.getElementById("flip-card-back").style.backgroundImage = 'url(./img/e' + x + '.png)';
+}
+}
 
-    element.classList.add("animation");
-}, false);
